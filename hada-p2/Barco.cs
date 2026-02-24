01132488832c7798c20fd9c13c31a6154713e2fd
Makita García <mgdr3@alu.ua.es>
2026-02-24 20:18:42 +0100

    54871932G Clase Barco comenzada

--- hada-p2/Barco.cs
+++ hada-p2/Barco.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace hada_p2
 {
-    internal class Barco
-    {
+    public class Barco
+    {   
+        //Propiedades
+        public Dictionary<Coordenada, string> coordenadasBarco { get; private set; }
+        public string Nombre { get; private set; }
+        public int NumDanyos { get; private set; }
+
+        // Eventos
+        public event EventHandler<TocadoArgs> eventoTocado;
+        public event EventHandler<HundidoArgs> eventoHundido;
     }
 }
