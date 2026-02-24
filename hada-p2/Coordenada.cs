b3ba63097c9a06ff117a53e2a6a772a68a216440
Makita García <mgdr3@alu.ua.es>
2026-02-24 19:53:08 +0100

    54871932G Comienzo clase Coordenada

--- hada-p2/Coordenada.cs
+++ hada-p2/Coordenada.cs
@@ -8,5 +8,45 @@ namespace hada_p2
 {
     internal class Coordenada
     {
+        private int _fila;
+        private int _columna;
+
+        // Propiedades con validación (0-9) 
+        public int Fila
+        {
+            get { return _fila; }
+            set { if (value >= 0 && value <= 9) _fila = value; }
+        }
+
+        public int Columna
+        {
+            get { return _columna; }
+            set { if (value >= 0 && value <= 9) _columna = value; }
+        }
+
+        // 4 Constructores requeridos
+        public Coordenada() { Fila = 0; Columna = 0; }
+
+        public Coordenada(int fila, int columna)
+        {
+            Fila = fila;
+            Columna = columna;
+        }
+
+        public Coordenada(string fila, string columna)
+        {
+            Fila = int.Parse(fila);
+            Columna = int.Parse(columna);
+        }
+
+        public Coordenada (Coordenada coordenada)
+        {
+            Fila = coordenada.Fila;
+            Columna = coordenada .Columna;
+        }
+
+        // Sobrecargas de métodos de objetos 
+
     }
+
 }
