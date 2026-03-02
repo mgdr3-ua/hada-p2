d7849ba95e45ed6434edff2920227a606265612f
lms121-ua <lms121@alu.ua.es>
2026-03-02 12:03:20 +0100

    X8594224K añadir estructuras internas a la clase Tablero

--- hada-p2/Tablero.cs
+++ hada-p2/Tablero.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hada;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,24 @@ namespace hada_p2
     internal class Tablero
     {
         //inicio
+        public Tablero(int tamTablero, List<Barco> barcos)
+        {
+
+        }
+
+        public void Disparar(Coordenada c)
+        {
+
+        }
+
+        public string DibujarTablero()
+        {
+            return "";
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
     }
 }
