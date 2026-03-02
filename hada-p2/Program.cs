66b95772fad3d0800b037d34762102ed199c9f49
lms121-ua <lms121@alu.ua.es>
2026-03-02 14:17:46 +0100

    X8594224K realizado la clase Program

--- hada-p2/Program.cs
+++ hada-p2/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace hada_p2
+namespace Hada
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Game game = new Game();
         }
     }
 }
